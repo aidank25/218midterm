7fdf387930648cf56d9b02f1f0a70b4063decad1
shmup <shmup@shmup-Virtual-Machine>
2024-11-10 17:15:14 -0500

    wrote unit tests for repl and help

--- app/calculator/test_calculator.py
+++ app/calculator/test_calculator.py
@@ -1,6 +1,22 @@
-from app.calculator import Calculator
+from app.calculator import Calculator, calculator_repl
 import pytest
+import sys
+from io import StringIO
 
 def test_perform_operation():
     calculator = Calculator()
     assert calculator.perform_operation("subtract", 3, 2) == 1
+
+def test_repl_start(capsys):
+    calculator_repl()
+    captured = capsys.readouterr()
+    assert "Calculator started. Type 'help' for commands.\n" in captured.out
+
+def test_help(monkeypatch,capsys):
+    monkeypatch.setattr(sys, "stdin", StringIO("help\n"))
+    calculator_repl()
+    captured = capsys.readouterr()
+    help_str = "help\nhistory\nclear\nundo\nredo\nsave\nload\nadd\nsubtract\nmultiply\ndivide"
+    assert help_str in captured.out
+
+    
